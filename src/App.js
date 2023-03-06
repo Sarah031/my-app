4d8d0903349f596189af6f770e37e18b8e94ad89
Sara Majstorović <sara.majstorovic01@gmail.com>
2023-03-06 20:22:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
+  const [godina, setGodina] = useState(2023);
+  const [klik, setKlik] = useState(0);
+  const init = useCallback(() => {
+    console.log("component init");
+  }, []);
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <p>{godina}</p>
+      <button
+        onClick={() => {
+          setGodina(godina + 1);
+        }}
+      >
+        dodaj broj
+      </button>
+      <p>{klik}</p>
+      <button
+        onClick={() => {
+          setKlik(klik + 1);
+        }}
+      >
+        klik
+      </button>
     </div>
   );
 }
